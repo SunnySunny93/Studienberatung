231ed9ca3ae6abee118c932d21180d31c4286622
Annika <you@example.com>
2014-10-23 11:06:27 +0200

    Ausgabe geaendert

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Studienberatung{
 	public static void main (String [] args){
 		Scanner scanner= new Scanner();
-		print.out.ln("Hast du Abitur?");
+		System.out.println("Hast du Abitur?");
 		String antwort = scanner.nextLine();
 	}
 }
