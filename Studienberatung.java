5d139689e427851f0da11ab3373a023635b7b939
Annika <you@example.com>
2014-10-23 11:02:11 +0200

    Abiturfrage hinzugefuegt

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Studienberatung{
 	public static void main (String [] args){
 		Scanner scanner= new Scanner();
+		print.out.ln("Hast du Abitur?");
+		String antwort = scanner.nextLine();
 	}
 }
